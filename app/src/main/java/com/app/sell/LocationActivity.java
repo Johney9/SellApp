5a2a0b3b7649afa6a940613695ef73dd4821fb9b
johney9 <nikola.mihalek@gmail.com>
2018-05-11 15:42:52 +0200

    added location saving on click

--- app/src/main/java/com/app/sell/LocationActivity.java
+++ app/src/main/java/com/app/sell/LocationActivity.java
@@ -1,12 +1,34 @@
 package com.app.sell;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import com.app.sell.dao.UserDao;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Bean;
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+import org.androidannotations.annotations.ViewsById;
+
+@EActivity
 public class LocationActivity extends AppCompatActivity {
 
     TextView mQuestion;
+
+    @ViewById(R.id.location_edit_text_location)
+    EditText locationEditText;
+
+    @ViewById(R.id.location_save_location_button)
+    Button saveLocationButton;
+
+    @Bean
+    UserDao userDao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,4 +42,13 @@ public class LocationActivity extends AppCompatActivity {
             }
         }
     }
+
+    @Click(R.id.location_save_location_button)
+    void saveLocation() {
+
+        userDao.getCurrentUser().setLocation(locationEditText.getText().toString());
+        userDao.write(userDao.getCurrentUser());
+        finish();
+    }
+
 }
