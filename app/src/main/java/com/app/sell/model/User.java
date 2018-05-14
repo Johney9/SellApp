742d04dea1106000d4909bfb78a6b5ddec7d4a19
johney9 <nikola.mihalek@gmail.com>
2018-05-14 10:21:55 +0200

    added lombok support. refactored.

--- app/src/main/java/com/app/sell/model/User.java
+++ app/src/main/java/com/app/sell/model/User.java
@@ -1,71 +1,21 @@
 package com.app.sell.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
+    private String uid;
     private String username;
     private String password;
     private String firstName;
     private String lastName;
     private String location;
     private String image;
+    private String email;
 
-    public User() {
-    }
-
-    public User(String username, String password, String firstName, String lastName, String location, String image) {
-        this.username = username;
-        this.password = password;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.location = location;
-        this.image = image;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
 }
