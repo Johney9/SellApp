05bea7607ae81e6d5ee43006733040ac4c997d1a
johney9 <nikola.mihalek@gmail.com>
2018-05-22 16:05:39 +0200

    implemented Firebase functions for sending of Cloud Messages for Chat. implemented auto assignment of ID when an object is added without an ID using Firebase push.

--- firebase-node/functions/index.js
+++ firebase-node/functions/index.js
@@ -2,9 +2,11 @@ let functions = require('firebase-functions');
 
 let admin = require('firebase-admin');
 
+let _ = require("underscore");
+
 admin.initializeApp(functions.config().firebase);
 
-exports.sendNotification = functions.database.ref('/chatrooms/{chatroomId}/chatroom_messages/{chatmessageId}')
+exports.sendChatNotification = functions.database.ref('/chatrooms/{chatroomId}/chatroom_messages/{chatmessageId}')
 .onWrite((snap, context) => {
 
 	console.log("System: starting");
@@ -28,15 +30,15 @@ exports.sendNotification = functions.database.ref('/chatrooms/{chatroomId}/chatr
 
 		//get the number of users in the chatroom
 		let length = 0;
-		for(value in data){
+		data.forEach((value) => {
 			length++;
-		}
+		});
 		console.log("data length: ", length);
 
 		//loop through each user currently in the chatroom
 		let tokens = [];
 		let i = 0;
-		for(var user_id in data){
+		data.forEach((user_id) => {
 			console.log("user_id: ", user_id);
 
 			//get the token and add it to the array
@@ -51,18 +53,17 @@ exports.sendNotification = functions.database.ref('/chatrooms/{chatroomId}/chatr
 				//also check to see if the user_id we're viewing is the user who posted the message
 				//if it is, then save that name so we can pre-pend it to the message
 				let messageUserName = "";
-				if(snap.child('user_id').val() == messageUserId){
+				if(snap.child('user_id').val() === messageUserId){
 					messageUserName = snap.child('name').val();
 					console.log("message user name: " , messageUserName);
 					message = messageUserName + ": " + message;
 				}
 
 				//Once the last user in the list has been added we can continue
-				if(i == length){
+				if(i === length) {
 
 					console.log("Construction the notification message.");
 					const payload = {
-
 						data: {
 							data_type: "type_chat_message",
 							title: "Prodaj!",
@@ -73,17 +74,42 @@ exports.sendNotification = functions.database.ref('/chatrooms/{chatroomId}/chatr
 
 
 					return admin.messaging().sendToDevice(tokens, payload)
-						.then(function(response) {
+						.then((response) => {
 							// See the MessagingDevicesResponse reference documentation for
 							// the contents of response.
 							console.log("Successfully sent message:", response);
+							return response;
 						  })
-						  .catch(function(error) {
+						  .catch((error) => {
 							console.log("Error sending message:", error);
 						  });
 				}
+				throw new Error("Error getting tokens!");
+			})
+			.catch((error) => {
+				console.log(error);
 			});
-
-		}
+		});
+	throw new Error("Fail!");
 	});
 });
+
+exports.autoAssignId = functions.database.ref("{parentNode}/{topId}")
+.onCreate((snap, context) => {
+	console.log("ID auto assignment starting");
+	let id = context.params.topId;
+	console.log("ID being processed: ", id);
+	let childNode = snap.val();
+	console.log("Node being processed: ", childNode);
+	if(!_.has(childNode, "id") && snap.child !== null) {
+		childNode.id = id;
+		return snap.ref.set(childNode)
+			.then(() => {
+				console.log("ID auto assignment successful.");
+				return 0;
+			})
+			.catch((error) => {
+				console.log("ID auto assignment reject with error: ", error);
+			});
+	}	
+})
